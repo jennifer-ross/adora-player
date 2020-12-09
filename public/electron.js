cf671c2148c63a37d295734a98c901e416f9b23e
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-12-09 03:47:03 +0300

    +electron

--- public/electron.js
+++ public/electron.js
@@ -1,12 +1,29 @@
 const electron = require("electron");
+const {ipcMain} = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
 const isDev = require("electron-is-dev");
+const express = require('express');
+const bodyParser = require('body-parser');
+let cors = require('cors');
+request = require('request');
+
 let mainWindow;
+let authWindow;
+let serverApp;
+let getCodeID;
+let server;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({ width: 900, height: 680 });
+    mainWindow = new BrowserWindow({
+        width: 1900,
+        height: 1600,
+        webPreferences: {
+            nodeIntegration: true,
+            webSecurity: false
+        },
+    });
     mainWindow.loadURL(
         isDev
             ? "http://localhost:3000"
@@ -14,8 +31,13 @@ function createWindow() {
     );
     mainWindow.on("closed", () => (mainWindow = null));
 }
-app.on("ready", createWindow);
+app.on("ready", () => {
+    startServer();
+    createWindow();
+});
 app.on("window-all-closed", () => {
+    clearInterval(getCodeID);
+    server.close();
     if (process.platform !== "darwin") {
         app.quit();
     }
@@ -25,3 +47,48 @@ app.on("activate", () => {
         createWindow();
     }
 });
+
+function startServer() {
+    serverApp = express();
+    serverApp.use(cors());
+    serverApp.use(bodyParser.urlencoded({ extended: false }));
+    serverApp.use(bodyParser.json());
+
+    serverApp.post('/auth', function(req, res) {
+        console.log(req.body.url);
+        if (req.body.url) {
+            if (!authWindow) {
+                authWindow = new BrowserWindow({
+                    parent: mainWindow,
+                    modal: true,
+                    show: false,
+                    width: 900,
+                    height: 680,
+                    webPreferences: {
+                        nodeIntegration: true,
+                        webSecurity: false
+                    },
+                });
+                authWindow.loadURL(req.body.url);
+                authWindow.once('ready-to-show', () => {
+                    authWindow.show();
+                });
+
+                authWindow.webContents.on('did-navigate', (event, url) => {
+                    const pattern = 'https://oauth.yandex.ru/verification_code#';
+
+                    if (url.startsWith(pattern)) {
+                        const stringParams = url.replace(pattern, '');
+                        const params = JSON.parse('{"' + decodeURI(stringParams).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+                        res.json({success: true, payload: params});
+                        authWindow.close();
+                    }
+                });
+
+                authWindow.once('closed', () => {authWindow = null;});
+            }
+        }
+    });
+
+    server = serverApp.listen(3001);
+}
