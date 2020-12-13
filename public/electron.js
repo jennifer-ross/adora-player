d24ff41853d1c4a13bd1948b661ee092cff8e49a
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-12-13 03:43:59 +0300

    fixes/remove unused

--- public/electron.js
+++ public/electron.js
@@ -1,5 +1,5 @@
 const electron = require("electron");
-const {ipcMain, Tray, session} = require("electron");
+const {ipcMain, Tray, session, Menu} = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const path = require("path");
@@ -15,9 +15,9 @@ let serverApp;
 let getCodeID;
 let server;
 let isInit = false;
+let appIcon;
 
 function createWindow() {
-    let appIcon;
     if (!isDev) {
         appIcon = path.join(__dirname, "../build/favicon.ico");
     }else {
@@ -25,6 +25,8 @@ function createWindow() {
     }
 
     // const tray = new Tray(appIcon);
+
+    Menu.setApplicationMenu(null);
 
     mainWindow = new BrowserWindow({
         width: 1240,
@@ -69,7 +71,6 @@ app.on("ready", () => {
     createWindow();
 });
 app.on("window-all-closed", () => {
-    clearInterval(getCodeID);
     server.close();
     if (process.platform !== "darwin") {
         app.quit();
@@ -98,34 +99,18 @@ function startServer() {
                     height: 680,
                     center: true,
                     transparent: false,
-                    frame: false,
+                    frame: true,
                     hasShadow: true,
+                    icon: appIcon,
                     webPreferences: {
-                        nodeIntegration: true,
-                        webSecurity: false,
-                        enableRemoteModule: true
+                        nodeIntegration: false,
+                        webSecurity: true,
+                        enableRemoteModule: false
                     },
                 });
                 authWindow.loadURL(req.body.url);
                 authWindow.once('ready-to-show', () => {
                     authWindow.show();
-                    authWindow.webContents.on('did-finish-load', () => {
-                        authWindow.webContents.executeJavaScript(
-                            `document.body.insertAdjacentHTML("afterBegin",'<style>body{overflow-x:hidden;overflow-y:hidden;}.button{border:none;outline:0;text-transform:uppercase;text-decoration:none;-webkit-transition:.3s ease;transition:.3s ease;color:#9c9c9c;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;-webkit-appearance:none;-moz-appearance:none;appearance:none;border-radius:3px}.button:hover{color:#892cdc;cursor:pointer}.button__icon{margin-right:10px;font-size:16px}.window-control{-webkit-app-region:drag;position:relative;width:100%;padding:8px 5px 8px 20px;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:justify;-webkit-justify-content:space-between;-ms-flex-pack:justify;justify-content:space-between;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;background:#000;color:#dcdcdc;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}.window-control__title{font-size:14px;line-height:14px}.window-control__drag{width:100%;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-flex:1;-webkit-flex:1 1 auto;-ms-flex:1 1 auto;flex:1 1 auto;z-index:4}.window-control__actions{-webkit-app-region: no-drag;margin-right:25px;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex}.window-control__actions .button{font-size:12px;line-height:12px;width:14px;height:14px;margin:0;padding:6px;display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-box-pack:center;-webkit-justify-content:center;-ms-flex-pack:center;justify-content:center;-webkit-box-align:center;-webkit-align-items:center;-ms-flex-align:center;align-items:center;z-index:1}.window-control__actions .button svg{width:14px;height:14px}.window-control__actions .button svg path{fill:#dcdcdc}.window-control__actions .button:hover{background:#892cdc;color:#fafafa}.window-control__actions .button:hover svg path{fill:#fafafa}.window-control__actions .btn-close{font-size:16px;line-height:16px}.window-control__actions .btn-close:hover{background:#e32929;color:#fafafa}</style><div class="window-control"><div class="window-control__drag"><div class="window-control__title">Adora player</div></div><div class="window-control__actions"><div class="button btn-minimize"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M480 480H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h448c17.7 0 32 14.3 32 32s-14.3 32-32 32z"/></svg></div><div class="button btn-screen-resize"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 0H144c-26.5 0-48 21.5-48 48v48H48c-26.5 0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h320c26.5 0 48-21.5 48-48v-48h48c26.5 0 48-21.5 48-48V48c0-26.5-21.5-48-48-48zm-96 464H48V256h320v208zm96-96h-48V144c0-26.5-21.5-48-48-48H144V48h320v320z"/></svg></div><div class="button btn-close"><svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"/></svg></div></div></div>');
-                                    let close = document.querySelector('.window-control__actions .btn-close');
-                                    let resize = document.querySelector('.window-control__actions .btn-screen-resize');
-                                    let minimize = document.querySelector('.window-control__actions .btn-minimize');
-                                    
-                                    if(close && resize && minimize) {
-                                        let remote = window.require("electron").remote;
-                                        let win = remote.getCurrentWindow();
-                                        close.addEventListener("click", e => {if (win.closable) {window.close();}});
-                                        resize.addEventListener("click", e => {if (!win.isMaximized()){win.maximize();}else{win.unmaximize();}});
-                                        minimize.addEventListener("click", e => {if (win.minimizable) {win.minimize();}});
-                                    }
-                                    `
-                        ).then(result => {}).catch(err => {});
-                    });
                 });
 
                 authWindow.webContents.on('did-navigate', (event, url) => {
@@ -137,7 +122,7 @@ function startServer() {
                             const params = {};
                             cookies.map((cookie, key) => {
                                 if (cookie.name === 'yandex_login' || cookie.name === 'Session_id' || cookie.name === 'sessionid2' || cookie.name === 'sessguard') {
-                                    params[cookie.name] = {value: cookie.value, domain: '.yandex.ru', expirationDate: cookie.expirationDate}
+                                    params[cookie.name] = {value: cookie.value, domain: '.yandex.ru', expirationDate: cookie.expirationDate};
                                 }
                             });
                             res.json({success: true, payload: params});
