db28855db42aca73cc7c28eaa0f70f3819411172
pguczol23 <38167698+pguczol23@users.noreply.github.com>
2020-12-16 18:08:58 +0300

    update reducers

--- src/Reducers/nonMusicBlocksReducer.js
+++ src/Reducers/nonMusicBlocksReducer.js
@@ -1,12 +1,18 @@
 import {NON_MUSIC_BLOCKS} from "../Actions/const";
 
 const initialState = {
+    albumIds: [],
+    albums: [],
+    chartPositions: [],
+    chartType: '',
+    description: '',
+    typeForForm: '',
 };
 
 export default function nonMusicBlocksReducer(state = initialState, action) {
     switch (action.type) {
     case NON_MUSIC_BLOCKS.SET_NON_MUSIC_BLOCKS:
-        return {...state, ...action.payload};
+        return {...state, ...action.payload.chart};
     case NON_MUSIC_BLOCKS.CLEAR_NON_MUSIC_BLOCKS:
         return {...state};
     default:
--- src/Reducers/playerStateReducer.js
+++ src/Reducers/playerStateReducer.js
@@ -5,13 +5,18 @@ const initialState = {
     progress: 0,
     volume: 1,
     muted: false,
+    track: {},
+    playlist: [],
+    currentTime: 0,
 };
 
 export default function playerTrackReducer(state = initialState, action) {
     switch (action.type) {
     case PLAYER.SET_PLAYER_STATE:
+        localStorage.setItem('playerState', JSON.stringify(action.payload));
         return {...state, ...action.payload};
     case PLAYER.CLEAR_PLAYER_STATE:
+        localStorage.setItem('playerState', JSON.stringify(state));
         return {...state, isPaused: true};
     default:
         return state;
